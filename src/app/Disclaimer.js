1a4d9885620b28239826da0ba0a2ec3162520399
steveoh <sgourley@utah.gov>
2017-07-17 13:37:59 -0600

    show disclaimer always

--- src/app/Disclaimer.js
+++ src/app/Disclaimer.js
@@ -16,29 +16,13 @@ define([
     return declare([_WidgetBase, _TemplatedMixin], {
         templateString: template,
 
-        // dontShowMeKey: String
-        //      localStorage key
-        dontShowMeKey: 'dontShowParcelsDisclaimerDialog',
-
         postCreate: function () {
             console.log('app/Disclaimer:postCreate', arguments);
-
-            if (localStorage.getItem(this.dontShowMeKey) === 'true') {
-                return;
-            }
 
             $(this.modal).modal({
                 keyboard: false,
                 backdrop: 'static'
             });
-        },
-        onShowMeChange: function (evt) {
-            // summary:
-            //      don't show me this again checkbox has been changed
-            // evt: Event
-            console.log('app/Disclaimer:onShowMeChange', arguments);
-
-            localStorage.setItem(this.dontShowMeKey, evt.target.checked);
         }
     });
 });
