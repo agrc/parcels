ff19ff1b162bbb7ce32c0e9dc2cb03fcd377aefc
stdavis <stdavis@utah.gov>
2020-08-04 11:56:04 -0600

    Release v1.3.3

--- src/app/config.js
+++ src/app/config.js
@@ -28,7 +28,7 @@ define([
 
         // version.: String
         //      The version number.
-        version: '1.3.1',
+        version: '1.3.3',
 
         // apiKey: String
         //      The api key used for services on api.mapserv.utah.gov
