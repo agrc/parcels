444b885bb94bc81948ba1e0a5ee632ad301ae5bb
steveoh <sgourley@utah.gov>
2025-03-27 20:34:18 -0600

    chore: add statewide parcel layer

--- src/context/MapProvider.tsx
+++ src/context/MapProvider.tsx
@@ -1,11 +1,11 @@
 import Graphic from '@arcgis/core/Graphic';
 import MapView from '@arcgis/core/views/MapView';
 import { useGraphicManager } from '@ugrc/utilities/hooks';
-import PropTypes from 'prop-types';
-import { createContext, ReactNode, useState } from 'react';
+import { createContext, type ReactNode, useCallback, useState } from 'react';
 
 type GraphicOptions = Graphic | Graphic[] | null;
 export const MapContext = createContext<{
+  addLayers: (layers: __esri.Layer[]) => void;
   mapView: MapView | null;
   setMapView: (mapView: MapView | null) => void;
   placeGraphic: (graphic: GraphicOptions) => void;
@@ -30,6 +30,25 @@ export const MapProvider = ({ children }: { children: ReactNode }) => {
     setGraphic(graphic);
   };
 
+  const addLayers = useCallback(
+    (layers: __esri.Layer[]): void => {
+      if (!mapView) {
+        console.warn('attempting to add a layer before the mapView is set');
+
+        return;
+      }
+
+      if (!mapView.map) {
+        console.warn('mapView does not have a map property');
+
+        return;
+      }
+
+      mapView.map.addMany(layers);
+    },
+    [mapView],
+  );
+
   return (
     <MapContext.Provider
       value={{
@@ -37,13 +56,10 @@ export const MapProvider = ({ children }: { children: ReactNode }) => {
         setMapView,
         placeGraphic,
         zoom,
+        addLayers,
       }}
     >
       {children}
     </MapContext.Provider>
   );
-};
-
-MapProvider.propTypes = {
-  children: PropTypes.node.isRequired,
 };
