094c9b210a4efdc8610481cbe8f4854d6a6a7c2a
steveoh <sgourley@utah.gov>
2025-03-31 17:23:59 -0600

    chore: dedupe arcgis when linking

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import loadVersion from 'vite-plugin-package-version';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), loadVersion()],
+  resolve: {
+    dedupe: ['@arcgis/core'],
+  },
 });
