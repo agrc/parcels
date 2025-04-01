0a7537a842374ec8bc016eede1bc3e540f72cb2f
steveoh <sgourley@utah.gov>
2025-04-01 10:11:44 -0600

    chore: remove unnecessary code

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,4 @@ import loadVersion from 'vite-plugin-package-version';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), loadVersion()],
-  resolve: {
-    dedupe: ['@arcgis/core'],
-  },
 });
